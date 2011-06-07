ea875e2551b9e38266452dbed63c2056fbf49683
Andy Balaam <andybalaam@artificialworlds.net>
2011-06-07 03:00:59 +0100

    Add a commented failing test for using a function in a full program.

--- src/test/testrendering.py
+++ src/test/testrendering.py
@@ -22,3 +22,42 @@ int main( int argc, char* argv[] )
 }
 """ )
 
+#def test_Echo_arg1():
+#	env = EeyEnvironment( EeyCppRenderer() )
+#	builtins.add_builtins( env )
+#
+#	# import sys
+#	#
+#	# def string getname( string name ):
+#	#     return name
+#	#
+#	# print getname( sys.argv[1] )
+#
+#	impt = EeyImport( "sys" )
+#
+#	fndef = EeyDefine( EeySymbol( "getname" ),
+#		EeyUserFunction(
+#			EeyType( EeyString ),
+#			(
+#				( EeyType( EeyString ), EeySymbol( "name" ) ),
+#				),
+#			(
+#				EeyReturn( EeySymbol( "name" ) ),
+#				)
+#			)
+#		)
+#
+#	fncall = EeyFunctionCall( EeySymbol( "print" ),
+#		( EeyFunctionCall( EeySymbol( "getname" ),
+#			EeyArrayLookup( EeySymbol( "sys.argv" ), 1 ) ), ) )
+#
+#	assert_equal( env.render_exe( ( value, ) ), """#include <stdio.h>
+#
+#int main( int argc, char* argv[] )
+#{
+#	printf( "Hello, World!\\n" );
+#
+#	return 0;
+#}
+#""" )
+
