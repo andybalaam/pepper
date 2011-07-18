5f6ba1b3a1f2798df1e17d2c29785f04939bd847
Andy Balaam <andybalaam@artificialworlds.net>
2011-07-18 21:29:03 +0100

    Test line numbers in lexer tests.

--- src/test/testlexer.py
+++ src/test/testlexer.py
@@ -7,17 +7,21 @@ from parse import EeyoreLexer
 def _lex( string ):
     return list( EeyoreLexer.Lexer( StringIO( string ) ) )
 
-def _assert_token( token, ( text, tp ) ):
+def _assert_token( token, text, tp, line = None, col = None ):
     assert_equal( token.getText(), text )
     assert_equal( token.getType(), tp )
+    if line is not None:
+        assert_equal( token.getLine(), line )
+    if col is not None:
+        assert_equal( token.getColumn(), col )
 
 def test_hello_world():
     tokens = _lex( """print( "Hello, world!" )""" )
 
-    _assert_token( tokens[0], ( "print",         EeyoreLexer.SYMBOL ) )
-    _assert_token( tokens[1], ( "(",             EeyoreLexer.LPAREN ) )
-    _assert_token( tokens[2], ( "Hello, world!", EeyoreLexer.STRING ) )
-    _assert_token( tokens[3], ( ")",             EeyoreLexer.RPAREN ) )
+    _assert_token( tokens[0], "print",         EeyoreLexer.SYMBOL, 1, 1 )
+    _assert_token( tokens[1], "(",             EeyoreLexer.LPAREN, 1, 6 )
+    _assert_token( tokens[2], "Hello, world!", EeyoreLexer.STRING, 1, 8 )
+    _assert_token( tokens[3], ")",             EeyoreLexer.RPAREN, 1, 24 )
 
-# TODO: test line numbers
+    assert_equal( len( tokens ), 4 )
 
