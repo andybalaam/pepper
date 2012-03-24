27e7577881476f21360cae40aaf619e543047b1b
Andy Balaam <andybalaam@artificialworlds.net>
2012-03-24 23:15:26 +0000

    Move more tests to the new parser test format.

---
+++ src/test/parser/test_if.py
@@ -0,0 +1,215 @@
+
+from assert_parser_result import assert_parser_result
+
+def test_if():
+    assert_parser_result(
+        r"""
+0001:0001       "if"(if)
+0001:0004     SYMBOL(True)
+0001:0008      COLON(:)
+0001:0009    NEWLINE
+0002:0001     INDENT
+0002:0005     SYMBOL(print)
+0002:0010     LPAREN
+0002:0012        INT(3)
+0002:0014     RPAREN
+0002:0015    NEWLINE
+0002:0015     DEDENT
+0002:0015    NEWLINE
+0003:0001    NEWLINE
+""",
+        r"""
+["if":if]
+    [SYMBOL:True]
+    [COLON::]
+        [LPAREN:]
+            [SYMBOL:print]
+            [INT:3]
+""",
+        r"""
+EeyIf(
+    EeySymbol('True'),
+    (
+        EeyFunctionCall(
+            EeySymbol('print'),
+            (
+                EeyInt('3'),
+            )
+        ),
+    ),
+    None
+)
+""" )
+
+
+def test_if_function_call():
+    assert_parser_result(
+        r"""
+0001:0001       "if"(if)
+0001:0004     SYMBOL(f)
+0001:0005     LPAREN
+0001:0007        INT(3)
+0001:0009     RPAREN
+0001:0010      COLON(:)
+0001:0011    NEWLINE
+0002:0001     INDENT
+0002:0005        INT(3)
+0002:0006    NEWLINE
+0002:0006     DEDENT
+0002:0006    NEWLINE
+0003:0001    NEWLINE
+""",
+        r"""
+["if":if]
+    [LPAREN:]
+        [SYMBOL:f]
+        [INT:3]
+    [COLON::]
+        [INT:3]
+""",
+        r"""
+EeyIf(
+    EeyFunctionCall(
+        EeySymbol('f'),
+        (
+            EeyInt('3'),
+        )
+    ),
+    (
+        EeyInt('3'),
+    ),
+    None
+)
+""" )
+
+
+
+def test_if_operator():
+    assert_parser_result(
+        r"""
+0001:0001       "if"(if)
+0001:0004        INT(3)
+0001:0006         GT(>)
+0001:0008        INT(4)
+0001:0009      COLON(:)
+0001:0010    NEWLINE
+0002:0001     INDENT
+0002:0005        INT(3)
+0002:0006    NEWLINE
+0002:0006     DEDENT
+0002:0006    NEWLINE
+0003:0001    NEWLINE
+""",
+        r"""
+["if":if]
+    [GT:>]
+        [INT:3]
+        [INT:4]
+    [COLON::]
+        [INT:3]
+""",
+        r"""
+EeyIf(
+    EeyGreaterThan(
+        EeyInt('3'),
+        EeyInt('4')
+    ),
+    (
+        EeyInt('3'),
+    ),
+    None
+)
+""" )
+
+
+def test_if_operator_and_function():
+    assert_parser_result(
+        r"""
+0001:0001       "if"(if)
+0001:0004     SYMBOL(f)
+0001:0005     LPAREN
+0001:0007     SYMBOL(a)
+0001:0009     RPAREN
+0001:0011         GT(>)
+0001:0013        INT(4)
+0001:0014      COLON(:)
+0001:0015    NEWLINE
+0002:0001     INDENT
+0002:0005        INT(3)
+0002:0006    NEWLINE
+0002:0006     DEDENT
+0002:0006    NEWLINE
+0003:0001    NEWLINE
+""",
+        r"""
+["if":if]
+    [GT:>]
+        [LPAREN:]
+            [SYMBOL:f]
+            [SYMBOL:a]
+        [INT:4]
+    [COLON::]
+        [INT:3]
+""",
+        r"""
+EeyIf(
+    EeyGreaterThan(
+        EeyFunctionCall(
+            EeySymbol('f'),
+            (
+                EeySymbol('a'),
+            )
+        ),
+        EeyInt('4')
+    ),
+    (
+        EeyInt('3'),
+    ),
+    None
+)
+""" )
+
+
+def test_if_else():
+
+    assert_parser_result(
+        r"""
+0001:0001       "if"(if)
+0001:0004     SYMBOL(True)
+0001:0008      COLON(:)
+0001:0009    NEWLINE
+0002:0001     INDENT
+0002:0005        INT(1)
+0002:0006    NEWLINE
+0002:0006     DEDENT
+0002:0006    NEWLINE
+0003:0001     "else"(else)
+0003:0005      COLON(:)
+0003:0006    NEWLINE
+0004:0001     INDENT
+0004:0005        INT(0)
+0004:0006    NEWLINE
+0004:0006     DEDENT
+0004:0006    NEWLINE
+0005:0001    NEWLINE
+""",
+        r"""
+["if":if]
+    [SYMBOL:True]
+    [COLON::]
+        [INT:1]
+    ["else":else]
+    [COLON::]
+        [INT:0]
+""",
+        r"""
+EeyIf(
+    EeySymbol('True'),
+    (
+        EeyInt('1'),
+    ),
+    (
+        EeyInt('0'),
+    )
+)""" )
+
---
+++ src/test/parser/test_operators.py
@@ -0,0 +1,95 @@
+from assert_parser_result import assert_parser_result
+
+def test_operator_plus():
+    assert_parser_result(
+        r"""
+0001:0001     SYMBOL(a)
+0001:0002       PLUS(+)
+0001:0003     SYMBOL(b)
+0001:0004    NEWLINE
+""",
+        r"""
+[PLUS:+]
+    [SYMBOL:a]
+    [SYMBOL:b]
+""",
+        r"""
+EeyPlus(
+    EeySymbol('a'),
+    EeySymbol('b')
+)
+""" )
+
+def test_operator_times():
+    assert_parser_result(
+        r"""
+0001:0001     SYMBOL(a)
+0001:0002      TIMES(*)
+0001:0003     SYMBOL(b)
+0001:0004    NEWLINE
+""",
+        r"""
+[TIMES:*]
+    [SYMBOL:a]
+    [SYMBOL:b]
+""",
+        r"""
+EeyTimes(
+    EeySymbol('a'),
+    EeySymbol('b')
+)
+""" )
+
+
+def test_operator_greater_than():
+    assert_parser_result(
+        r"""
+0001:0001     SYMBOL(a)
+0001:0002         GT(>)
+0001:0003     SYMBOL(b)
+0001:0004    NEWLINE
+""",
+        r"""
+[GT:>]
+    [SYMBOL:a]
+    [SYMBOL:b]
+""",
+        r"""
+EeyGreaterThan(
+    EeySymbol('a'),
+    EeySymbol('b')
+)
+""" )
+
+
+def test_plus_in_function_call():
+    assert_parser_result(
+        r"""
+0001:0001     SYMBOL(print)
+0001:0006     LPAREN
+0001:0008        INT(3)
+0001:0010       PLUS(+)
+0001:0012     SYMBOL(b)
+0001:0014     RPAREN
+0001:0015    NEWLINE
+""",
+        r"""
+[LPAREN:]
+    [SYMBOL:print]
+    [PLUS:+]
+        [INT:3]
+        [SYMBOL:b]
+""",
+        r"""
+EeyFunctionCall(
+    EeySymbol('print'),
+    (
+        EeyPlus(
+            EeyInt('3'),
+            EeySymbol('b')
+        ),
+    )
+)
+""" )
+
+
