5e70918ef77aa276a5ad375da1e628eaca7be73d
Andy Balaam <andybalaam@artificialworlds.net>
2012-11-04 03:04:51 +0000

    Add an example that appears to demonstrate I want to understand functions where some arguments are known.

---
+++ www/samples/reduce/reduce.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int reduce( int end )
+{
+    int acc = 1;
+    for( int v = 1; v < end; ++v )
+    {
+        acc *= v;
+    }
+    return acc;
+}
+
+int main( int argc, char* argv[] )
+{
+    printf( "%d\n", 12 );
+    printf( "%d\n", reduce( argc + 1 ) );
+
+    return 0;
+}
