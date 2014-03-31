b45383be1ebfcc301b4b5993ddd25936eee1db48
Andy Balaam <andybalaam@artificialworlds.net>
2014-04-01 00:51:41 +0100

    Remove debug print

--- src/libpepper/vals/basic_types/pepmodification.py
+++ src/libpepper/vals/basic_types/pepmodification.py
@@ -15,7 +15,6 @@ class PepModification( PepValue ):
 
     def do_evaluate( self, env ):
         if self.is_known( env ):
-            print "is_known"
             namespace, name, base_name = self.var.find_namespace_and_name( env )
             namespace[name].plusequals( self.mod_value.evaluate( env ) )
         return self
