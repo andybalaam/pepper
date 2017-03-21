5a73b9aee22d8fab87ec82d7edab9b9796e23cdb
Andy Balaam <andybalaam@artificialworlds.net>
2017-03-21 08:14:00 +0000

    Allow type_check to take a raw type

--- type_check.py
+++ type_check.py
@@ -1,14 +1,18 @@
 from pltypes.hasmethod import hasmethod
+from pltypes.pltypeequals import plTypeEquals
 from pltypeerror import plTypeError
 
 
 def type_check(type_, var_value, var_name):
-    if not hasmethod(type_, "check"):
+    assert type(var_name) == str
+    if type(type_) == type:
+        type_check(plTypeEquals(type_), var_value, var_name)
+    elif hasmethod(type_, "check"):
+        type_.check(var_value, var_name)
+    else:
         raise plTypeError(
             "type_",
             type(type_).__name__,
             "Checkable or type",
             type_
         )
-    assert type(var_name) == str
-    type_.check(var_value, var_name)
